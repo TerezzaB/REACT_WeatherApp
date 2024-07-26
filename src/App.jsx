a8ebc4c72fbe07b1e8a5b9669ba1e2b7e74e68d9
Terézia Boďová <bodova.terezia13@gmail.com>
2024-07-26 15:00:48 +0200

    2nd commit - Basic design + nav + tailwind installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import InfoModal from './components/InfoModal';
+import './App.css';
+import HomeView from './views/HomeView';
+import WeatherView from './views/WeatherView';
+import modalContent from './assets/modalContent.json';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [infoContent, setInfoContent] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  useEffect(() => {
+    setInfoContent(modalContent.infoText);
+  }, []);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return(
+    <Router>
+    <div>
+    <div className="sticky top-0 flex justify-between items-center p-2 _nav">
+        <div className="flex-1 flex justify-center space-x-4">
+          <NavLink to="/" className={({ isActive }) => `hover:text-purple-500 ${isActive ? 'text-purple-700 font-bold underline' : ''}`}>Home</NavLink>
+          <NavLink to="/weather" className={({ isActive }) => `hover:text-purple-500 ${isActive ? 'text-purple-700 font-bold underline' : ''}`}>Weather</NavLink>
+        </div>
+        <div className="p-2">
+          <FontAwesomeIcon icon={faInfoCircle} className="hover:text-purple-500 cursor-pointer" onClick={handleOpenModal} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/weather" element={<WeatherView />} />
+      </Routes>
+      <InfoModal isOpen={isModalOpen} onClose={handleCloseModal} content={infoContent} />
+    </div>
+  </Router>
   )
 }
-
-export default App
---
+++ src/views/WeatherView.jsx
@@ -0,0 +1,7 @@
+export default function WeatherView() {
+    return(
+        <>
+            <h1>Weather View</h1>
+        </>
+    )
+}
